96c65f874069b947a2c1889877eea3957fc533e7
Jacox4 <javier.andres.cc@gmail.com>
2019-07-08 19:24:37 -0400

    a

--- src/Login.java
+++ src/Login.java
@@ -3,4 +3,5 @@ import javax.swing.*;
 public class Login {
     private JPasswordField passwordPasswordField;
     private JFormattedTextField formattedTextField1;
+    private JButton loginButton;
 }
