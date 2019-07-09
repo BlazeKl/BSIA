9b541309f0898b1509be079a545b4cb5ae42e5cc
BlazeKl <felipe.perezc@utem.cl>
2019-07-09 00:32:04 +0000

    ventanas

--- src/bsia/BSIA.java
+++ src/bsia/BSIA.java
@@ -15,7 +15,7 @@ public class BSIA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Login.main(args);// TODO code application logic here
     }
     
 }
